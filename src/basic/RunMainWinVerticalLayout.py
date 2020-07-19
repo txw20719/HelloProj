a6f1d9e2eb905a3230f71ba50e352bb999171527
txw20719 <15755337992@163.com>
2020-07-19 19:42:38 +0800

    垂直栅格布局

---
+++ src/basic/RunMainWinVerticalLayout.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+# 代码文件：HelloProj/src/basic/RunMainWinHorizontalLayout.py
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+from src.designer.MainWinVerticalLayout import Ui_MainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWin = QMainWindow()
+    ui = Ui_MainWindow()
+    # 向主窗口添加控件
+    ui.setupUi(mainWin)
+    mainWin.show()
+    sys.exit(app.exec_())
