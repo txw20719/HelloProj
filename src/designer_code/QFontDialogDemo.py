fe4176c7f1af879423a966c18f633f527290e995
txw20719 <15755337992@163.com>
2020-08-05 19:21:16 +0800

    fontDialog

---
+++ src/designer_code/QFontDialogDemo.py
@@ -0,0 +1,40 @@
+# encoding=utf-8
+# 代码文件：HelloProj/src/designer_code/QFontDialog.py
+
+
+import sys
+from PyQt5.QtWidgets import QFontDialog, QLabel, QWidget, QApplication, QVBoxLayout, QPushButton
+
+
+class QFontDialogDemo(QWidget):
+    def __init__(self):
+        super(QFontDialogDemo, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('字体对话框')
+        self.resize(300, 280)
+
+        layout = QVBoxLayout()
+
+        self.button = QPushButton('选择字体')
+        self.button.clicked.connect(self.SelectFont)
+        layout.addWidget(self.button)
+
+        self.label = QLabel()
+        self.label.setText('Hello World!')
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+    def SelectFont(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.label.setFont(font)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QFontDialogDemo()
+    main.show()
+    sys.exit(app.exec_())
